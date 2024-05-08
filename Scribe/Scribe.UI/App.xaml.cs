dc37ab48a45f75a456963d400c234f2530404c97
Enzzo Morais de Olinda <enzzomorais0@gmail.com>
2024-05-07 22:59:57 -0300

    Initial commit

---
+++ Scribe/Scribe.UI/App.xaml.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+
+namespace Scribe.UI;
+
+public partial class App : Application;
---
+++ Scribe/Scribe.UI/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Scribe.UI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
